596ef2c7e4357143e691f4e730a5b4fce62aa422
AmoonPod <amoonpod@gmail.com>
2023-06-18 19:31:55 +0200

    change image importing

--- constants/projects.tsx
+++ constants/projects.tsx
@@ -1,22 +1,26 @@
+import ewo from '../public/ewo_crm.jpg';
+import assetplan from '../public/assetplan_website.jpg';
+import bluehorizon from '../public/blue_horizon_website.jpg';
+import manuel from '../public/manuel_old_website.jpg';
 export const projects = [
   {
     id: 1,
     title: 'EWO CRM',
-    coverImage: '/../public/ewo.jpg',
+    coverImage: ewo,
   },
   {
     id: 2,
     title: "Asset Plan's website",
-    coverImage: '/../public/assetplan_website.jpg',
+    coverImage: assetplan,
   },
   {
     id: 3,
     title: 'Blue Horizon Expeditions',
-    coverImage: '/../public/blue_horizon_website.jpg',
+    coverImage: bluehorizon,
   },
   {
     id: 4,
     title: 'Manuel De Ceglie (old website)',
-    coverImage: '/../public/manuel_old_website.jpg',
+    coverImage: manuel,
   },
 ];
